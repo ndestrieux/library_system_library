cc5a5f20df03490380d4590e52bd1f3a27106a8d
ndestrieux <n.destrieux@gmail.com>
2024-06-21 20:18:23 +0200

    sqlite or postgresql database can be used, URL to database will be built based on choice from .env file.

--- src/conf.py
+++ src/conf.py
@@ -5,8 +5,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
-
-    DATABASE_URL = str
+    which_db: str
+    db_username: str = ""
+    db_password: str = ""
+    db_name: str
 
 
 @lru_cache()
