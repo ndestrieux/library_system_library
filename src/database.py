18c38a7260211fd87515773362f30fae6ecec1e5
Nicolas Destrieux <42584510+ndestrieux@users.noreply.github.com>
2024-06-25 21:46:42 +0200

    Create db models (#3)
    * sqlite3 extension added to gitignore.
    
    * sqlite or postgresql database can be used, URL to database will be built based on choice from .env file.
    
    * Book and Author models created.
    
    * Database initialization added to main.py and get_db() function added to database file.

--- src/database.py
+++ src/database.py
@@ -4,8 +4,23 @@ from sqlalchemy.orm import sessionmaker
 
 from conf import get_settings
 
-engine = create_engine(get_settings().DATABASE_URL)
+DB_CHOICES = {
+    "sqlite": f"sqlite:///./{get_settings().db_name}",
+    "postgresql": f"postgresql://{get_settings().db_username}:{get_settings().db_password}@{get_settings().db_name}/db",
+}
+
+DATABASE_URL = DB_CHOICES[get_settings().which_db]
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
