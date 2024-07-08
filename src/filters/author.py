4661af0b907a46211566b58f0412a8aa9c1cbeaa
ndestrieux <n.destrieux@gmail.com>
2024-07-08 14:24:02 +0200

    Second filter created for filtering authors by ID.

--- src/filters/author.py
+++ src/filters/author.py
@@ -3,9 +3,18 @@ from typing import Optional
 import strawberry
 
 
-@strawberry.input
 class AuthorFilter:
+    pass
+
+
+@strawberry.input
+class AllAuthorFilter(AuthorFilter):
     first_name: Optional[str] = strawberry.UNSET
     middle_name: Optional[str] = strawberry.UNSET
     last_name: Optional[str] = strawberry.UNSET
     book_title: Optional[str] = strawberry.UNSET
+
+
+@strawberry.input
+class OneAuthorFilter(AuthorFilter):
+    id: Optional[int] = strawberry.UNSET
