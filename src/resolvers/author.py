e15a3ffc81aa8c1a0d55fb6279a9b39d0f522ec4
ndestrieux <n.destrieux@gmail.com>
2024-07-08 14:25:52 +0200

    - author_details query added - type hints added - model argument removed when initiating AuthorQuery

--- src/resolvers/author.py
+++ src/resolvers/author.py
@@ -1,16 +1,27 @@
 from typing import Optional
 
+from sqlalchemy import ScalarResult
 from sqlalchemy.orm import Session
 from strawberry import Info
 
-from filters.author import AuthorFilter
+from filters.author import AllAuthorFilter, OneAuthorFilter
 from models import Author as AuthorModel
 from query_builders import AuthorQuery
 
 
-def get_authors(db: Session, info: Info, f: Optional[AuthorFilter]):
+def get_authors(
+    db: Session, info: Info, f: Optional[AllAuthorFilter]
+) -> ScalarResult[AuthorModel]:
     required_fields = info.selected_fields[0].selections
-    query_obj = AuthorQuery(AuthorModel, required_fields, f)
+    query_obj = AuthorQuery(required_fields, f)
     query = query_obj.build()
     result = db.execute(query).unique()
     return result.scalars()
+
+
+def get_author_details(db: Session, info: Info, f: OneAuthorFilter) -> AuthorModel:
+    required_fields = info.selected_fields[0].selections
+    query_obj = AuthorQuery(required_fields, f)
+    query = query_obj.build()
+    result = db.execute(query).unique()
+    return result.scalar()
--- src/schema.py
+++ src/schema.py
@@ -4,16 +4,22 @@ import strawberry
 from strawberry import Info
 
 from definitions.author import Author
-from filters.author import AuthorFilter
-from resolvers.author import get_authors
+from filters.author import AllAuthorFilter, OneAuthorFilter
+from resolvers.author import get_author_details, get_authors
 
 
 @strawberry.type
 class Query:
     @strawberry.field
     async def author_list(
-        self, info: Info, f: Optional[AuthorFilter] = None
+        self, info: Info, f: Optional[AllAuthorFilter] = None
     ) -> List[Author]:
         db = info.context["db"]
         authors = get_authors(db, info, f)
         return [Author.from_instance(author) for author in authors]
+
+    @strawberry.field
+    async def author_details(self, info: Info, f: Optional[OneAuthorFilter]) -> Author:
+        db = info.context["db"]
+        author = get_author_details(db, info, f)
+        return Author.from_instance(author)
