1ea4bef50e13d557697ab18080ad42df4d77e06d
ndestrieux <n.destrieux@gmail.com>
2024-07-08 12:23:41 +0200

    - author router added to main app - database connection handling added on startup and shutdown.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 
 from database import engine
 from models import Base
-
-Base.metadata.create_all(bind=engine)
+from routers.author import router as author_router
 
 app = FastAPI()
+
+app.include_router(author_router)
+
+
+@app.on_event("startup")
+async def startup():
+    Base.metadata.create_all(bind=engine)
+    print("Database connected on startup")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    engine.dispose()
+    print("Database disconnected on shutdown")
