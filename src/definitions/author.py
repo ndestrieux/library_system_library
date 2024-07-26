9c3f52a5d96e625f0220ff3298311f2524d81a0e
ndestrieux <n.destrieux@gmail.com>
2024-07-26 15:02:14 +0200

    Model definition for Author for admin use added.

--- src/definitions/author.py
+++ src/definitions/author.py
@@ -1,24 +1,21 @@
+from datetime import date
 from typing import Annotated, List, Optional
 
 import strawberry
 
-from database.models import Author as AuthorModel
-
 
 @strawberry.type
-class Author:
+class AuthorBasic:
     id: int
     first_name: str
     middle_name: Optional[str]
     last_name: str
     books: List[Annotated["Book", strawberry.lazy(".book")]]
 
-    @classmethod
-    def from_instance(cls, obj: AuthorModel) -> "Author":
-        return cls(
-            id=obj.id,
-            first_name=obj.first_name,
-            middle_name=obj.middle_name,
-            last_name=obj.last_name,
-            books=obj.books,
-        )
+
+@strawberry.type
+class AuthorAdmin(AuthorBasic):
+    created_by: str
+    created_on: date
+    last_updated_by: Optional[str]
+    last_updated_on: Optional[date]
