e5c2ed97d785853aa1773bfe64fb707354001146
ndestrieux <n.destrieux@gmail.com>
2024-08-06 19:10:27 +0200

    Definitions (for basic and admin users) created for Book model.

--- src/definitions/author.py
+++ src/definitions/author.py
@@ -1,7 +1,8 @@
-from datetime import date
 from typing import Annotated, List, Optional
 
 import strawberry
+
+from definitions.base import AdminExtraFields
 
 
 @strawberry.type
@@ -10,12 +11,9 @@ class AuthorBasic:
     first_name: str
     middle_name: Optional[str]
     last_name: str
-    books: List[Annotated["Book", strawberry.lazy(".book")]]
+    books: List[Annotated["BookBasic", strawberry.lazy(".book")]]
 
 
 @strawberry.type
-class AuthorAdmin(AuthorBasic):
-    created_by: str
-    created_on: date
-    last_updated_by: Optional[str]
-    last_updated_on: Optional[date]
+class AuthorAdmin(AuthorBasic, AdminExtraFields):
+    books: List[Annotated["BookAdmin", strawberry.lazy(".book")]]
--- src/definitions/book.py
+++ src/definitions/book.py
@@ -1,6 +1,19 @@
+from typing import Annotated, List, Optional
+
 import strawberry
+
+from definitions.base import AdminExtraFields
 
 
 @strawberry.type
-class Book:
+class BookBasic:
     title: str
+    authors: List[Annotated["AuthorBasic", strawberry.lazy(".author")]]
+    publication_year: int
+    language: str
+    category: Optional[str]
+
+
+@strawberry.type
+class BookAdmin(BookBasic, AdminExtraFields):
+    authors: List[Annotated["AuthorAdmin", strawberry.lazy(".author")]]
