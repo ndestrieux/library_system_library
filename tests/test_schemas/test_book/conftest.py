7c14c7d14b9e2ab6a2a8d799b04b9536cf17676b
ndestrieux <n.destrieux@gmail.com>
2024-08-19 22:16:22 +0200

    Tests for bookList GraphQL query created.

---
+++ tests/test_schemas/test_book/conftest.py
@@ -0,0 +1,72 @@
+from datetime import date
+
+import pytest
+from freezegun import freeze_time
+from strawberry.types.nodes import SelectedField
+
+from database.models import LanguageChoices
+from database.validators.author import AuthorCreateValidator
+from database.validators.book import BookCreateValidator
+from src.database.crud_factory import AuthorSQLCrud, BookSQLCrud
+
+
+@pytest.fixture(scope="session")
+def selected_fields():
+    return [SelectedField(name="id", arguments={}, directives={}, selections=[])]
+
+
+@pytest.fixture(scope="function")
+def author_validated_data_list():
+    return [
+        AuthorCreateValidator(
+            first_name="Bram", last_name="Stoker", created_by="admin"
+        ),
+        AuthorCreateValidator(
+            first_name="Louis-Ferdinand", last_name="Céline", created_by="admin"
+        ),
+        AuthorCreateValidator(
+            first_name="John",
+            middle_name="Ronald Reuel",
+            last_name="Tolkien",
+            created_by="admin",
+        ),
+    ]
+
+
+@pytest.fixture(scope="function")
+def book_data_list(db_session):
+    return [
+        {
+            "title": "Dracula",
+            "publication_year": 1897,
+            "language": LanguageChoices.EN,
+            "category": "Horror",
+            "created_by": "admin",
+        },
+        {
+            "title": "Voyage au bout de la nuit",
+            "publication_year": 1932,
+            "language": LanguageChoices.FR,
+            "category": "Novel",
+            "created_by": "admin",
+        },
+        {
+            "title": "The Hobbit",
+            "publication_year": 1937,
+            "language": LanguageChoices.EN,
+            "category": "Fantasy",
+            "created_by": "admin",
+        },
+    ]
+
+
+@pytest.fixture(scope="function")
+def populate_db(db_session, author_validated_data_list, book_data_list):
+    dates = (date(2024, month, 1) for month in range(1, 7))
+    for author, book in zip(author_validated_data_list, book_data_list):
+        with freeze_time(dates):
+            author_obj = AuthorSQLCrud.create(db_session, author)
+            book_validated_data = BookCreateValidator(**book)
+            book_obj = BookSQLCrud.create(db_session, book_validated_data)
+            book_obj.authors.append(author_obj)
+            db_session.commit()
