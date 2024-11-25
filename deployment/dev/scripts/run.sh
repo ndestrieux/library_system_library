7619c219eadd7fc385c5cc99e32d4d2de99b6ab3
Nicolas Destrieux <42584510+ndestrieux@users.noreply.github.com>
2024-11-25 21:50:55 +0100

    Create docker compose for dev and prod environments (#6)
    * Docker files created for dev environment with postgresql.
    
    * Docker files created for prod environment.
    
    * Use of COPY command reduced.
    
    * Pydantic setting variables made uppercase.

---
+++ deployment/dev/scripts/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -e
+
+fastapi dev --host 0.0.0.0 --port 8000
--- src/database/db_conf.py
+++ src/database/db_conf.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from conf import get_settings
 
 DB_CHOICES = {
-    "sqlite": f"sqlite:///./{get_settings().db_name}",
-    "postgresql": f"postgresql://{get_settings().db_username}:{get_settings().db_password}@{get_settings().db_name}/db",
+    "sqlite": f"sqlite:///./{get_settings().DB_NAME}",
+    "postgresql": f"postgresql://{get_settings().DB_USERNAME}:{get_settings().DB_PASSWORD}@{get_settings().DB_HOST}/{get_settings().DB_NAME}",
 }
 
-DATABASE_URL = DB_CHOICES[get_settings().which_db]
+DATABASE_URL = DB_CHOICES[get_settings().WHICH_DB]
 
 engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=0)
 
